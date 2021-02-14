0e1d068b5d4c0a780501f87d8034510a5ba428d6
nabsty55 <nabsty55@gmail.com>
2021-02-14 20:24:44 +0300

    is max min equal

---
+++ IsMaxMinEqual.java
@@ -0,0 +1,48 @@
+package practice;
+
+public class IsMaxMinEqual {
+	static int isMaxMinEqual(int[] a ) {
+		
+		if(a.length < 2)
+			return 0;
+		
+		int maxValue = a[0];
+		int minValue = a[0];
+		int countMaxValues = 0;
+		int countMinValues = 0;
+		int countDiffValues = 0;
+		
+		
+		
+		for(int i=0; i< a.length;i++) {
+			for(int j=0; j < a.length; j++) {
+				if(a[i] != a[j])
+					countDiffValues ++;
+			
+			}
+		}
+		for(int k= 0; k < a.length; k++) {
+			if(a[k]>maxValue)
+				maxValue = a[k];
+			if(a[k]< minValue)
+				minValue = a[k];
+		}
+		for(int l= 0; l < a.length; l++) {
+			if(a[l]== maxValue)
+				countMaxValues ++;
+			if(a[l] == minValue)
+				countMinValues ++;
+		}
+		if((countDiffValues > 0) && (countMaxValues == countMinValues))
+			return 1;
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {2,4,6,2,8,11,11};
+		System.out.println(isMaxMinEqual(arr));
+
+	}
+
+}
