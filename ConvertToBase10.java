a039a24e0770df2fe38db02561a21445da2a49df
nabsty55 <nabsty55@gmail.com>
2021-02-14 12:29:56 +0300

    converting to base 10

---
+++ ConvertToBase10.java
@@ -0,0 +1,35 @@
+package practice;
+
+public class ConvertToBase10 {
+	int isLegalNumber(int[] a, int n) {
+		int countIllegal = 0;
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] >= n)
+				countIllegal ++;
+		}
+		if(countIllegal == 0)
+			return 1;
+		return 0;
+	}
+	int convertToBase10(int[] a, int base) {
+		int sum = 0;
+		int multiplier = 1;
+		if(isLegalNumber(a, base) == 0)
+			return -1;
+		else
+			for(int i = a.length-1; i >= 0; i --) {
+				sum += a[i]*multiplier;
+				multiplier *=base;
+				
+			}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 2};
+		ConvertToBase10 ctb = new ConvertToBase10();
+		System.out.println(ctb.convertToBase10(arr, 3));
+
+	}
+
+}
