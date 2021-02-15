49ed3f4fef8cdd518decd633d1e5004269cb661d
nabsty55 <nabsty55@gmail.com>
2021-02-15 08:40:44 +0300

    hollow array

---
+++ IsHollow.java
@@ -0,0 +1,52 @@
+package practice;
+
+public class IsHollow {
+	static int isHollow(int[] a) {
+
+		
+		int countPreceeding = 0;
+		int countZeroes = 0;
+		int countFollowing = 0;
+		
+		for(int i= 0; i < a.length; i++ ) {
+			
+			if(a[i] != 0) {
+				countPreceeding ++;
+				continue;
+				
+			}
+			if(a[i] == 0) {
+				 for(int j=i; j < a.length; j++ ) {
+					if(a[j] == 0) {
+						countZeroes ++;
+						i++;
+						continue;
+					}
+					if(a[j] != 0) {
+						for(int k=i; k < a.length; k++) {
+							if(a[k] != 0) {
+								countFollowing ++;
+								i++;
+								continue;
+								
+								
+							}
+							if(a[k] == 0)
+								return 0;
+						}
+					}
+				}
+			}
+		}
+		if((countPreceeding == countFollowing) && countZeroes >= 3)
+			return 1;
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1,0,0,0,1, 1};
+		System.out.println(isHollow(arr));
+
+	}
+
+}
