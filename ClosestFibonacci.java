d878927872b4d3f83b3e5819c2d77b40c4439a7c
nabsty55 <nabsty55@gmail.com>
2021-02-25 18:20:21 +0300

    closest fibonacci done

---
+++ ClosestFibonacci.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class ClosestFibonacci {
+	static int closestFibonacci(int n) {
+		if(n < 1)
+			return 0;
+		int previous = 1;
+		int next = 1;
+		while(next < n) {
+			int temp = next;
+			next = previous + next;
+			previous = temp;
+		}
+		if(next > n)
+			return previous;
+		return next;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(closestFibonacci(13));
+
+	}
+
+}
