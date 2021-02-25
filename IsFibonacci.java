c74c1fa56a9033c3a4beed1148b85654a6356ace
nabsty55 <nabsty55@gmail.com>
2021-02-25 20:51:54 +0300

    is fibonacci done

---
+++ IsFibonacci.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class IsFibonacci {
+	static int isFibonacci(int n) {
+		int previous = 1;
+		int next = 1;
+		while(next < n) {
+			int temp = next;
+			next = previous + next;
+			previous = temp;
+		}
+		if(next == n)
+			return 1;
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isFibonacci(34));
+
+	}
+
+}
